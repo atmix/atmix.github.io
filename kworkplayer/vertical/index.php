28e782012f601df6e4e60e9c5065e1c27314a4eb
atmix <75258577+atmix@users.noreply.github.com>
2022-08-20 09:23:21 +0500

    Add files via upload

---
+++ kworkplayer/vertical/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ru" prefix="og: http://ogp.me/ns#">
+
+<head>
+	<link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="anonymous">
+	<title>Deep House Online Radio 24/7</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="c/style.css">
+	<meta name="description" content="Некоммерческая интернет радиостанция с популярной спокойной и расслабляющей электронной музыкой.">
+	<link rel="icon" href="i/favicon.svg" type="image/svg+xml">
+    <meta name="robots" content="noindex,nofollow,noarchive">
+</head>
+
+<body>
+	<section class="split-wrapper no-split">
+		<div class="split-container">
+			<div class="split-col col-right">
+				<div class="content bigplayer drop-shadow">
+					<div class="player" id="stream"></div>
+					<ul class="social-links">
+						<li><a href="https://instagram.com/mdeepradio" rel="nofollow"><i class="fa fa-instagram" aria-hidden="true"></i> Instagram</a></li>
+						<li><a href="https://t.me/dhtop" rel="nofollow"><i class="fa fa-telegram" aria-hidden="true"></i> Telegram</a></li>
+						<li><a href="https://vk.com/deepmania" rel="nofollow"><i class="fa fa-vk" aria-hidden="true"></i> Вконтакте</a></li>
+						<li><a href="https://mdeep.ru/donate" style="color:#75d4da"><i class="fa fa-star" aria-hidden="true"></i> Donate</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+	<script src="j/jquery.js"></script>
+	<script src="j/min.js"></script>
+</body>
+
+</html>
